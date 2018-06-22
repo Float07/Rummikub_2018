5c409b9215f016f2f5042b0f7d0ea688bad533cc
Unknown <murilo_leone@usp.br>
2018-06-22 00:27:08 -0300

    funções tabuleiro

--- tabuleiro.c
+++ tabuleiro.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include "Rummikub.h"
 
+<<<<<<< HEAD
 struct tabuleiro{
 	int n;
 	t_carta carta[13];
@@ -11,20 +12,36 @@ struct tabuleiro{
 
 typedef struct tabuleiro t_tabuleiro; 
 typedef t_tabuleiro *t_tabuleiro_ptr;
+=======
 /*t_tabuleiro_ptr criar_conjunto(t_tabuleiro_ptr novo) {
 	
+>>>>>>> master
 
+
+t_tabuleiro_ptr criar_conjunto(t_tabuleiro_ptr novo, int numpl) {
+	int aux;
+	int num;
+	printf("Quais cartas vc deseja adicionar?(no minimo 3)\n")
+	for (int i = 0; i < t_player.cards; ++i)
+	{
+		printf("%d. %c%c  ", i+1, player[numpl].carta[i].nbr, player[numpl].carta[i].cor )
+		
+	}
+ 	scanf("%d", num);
 }
+
 */
 void imprime_tabuleiro(t_tabuleiro_ptr conjunto){
 	for (int i = 0; i < conjunto->n; ++i){
 		printf("%c%c ", conjunto->carta[i].nbr, conjunto->carta[i].cor );
 	}
 	printf("\n");
-	if(conjunto->next != NULL){
+	if(conjunto->next != NULL)
+	{
 			imprime_tabuleiro(conjunto->next);
 	}
-	else{
+	else
+	{
 		return;
 	}
 	
